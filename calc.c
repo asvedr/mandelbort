55bee905c585606d5b716387cd3392c2f37421d1
Arseny <asvedr@gmail.com>
2017-02-10 10:28:08 +0300

    gradient C color

--- calc.c
+++ calc.c
@@ -25,6 +25,64 @@ inline static double julia(double x, double y) {
 	return (double)depth;
 }
 
+typedef struct {
+	double r;
+	double g;
+	double b;
+} ColorR;
+
+typedef struct {
+	int r;
+	int g;
+	int b;
+} ColorI;
+
+#define MAX_LEN 5
+static ColorR color[MAX_LEN];
+static int color_len = 0;
+
+inline static ColorI get_color(double x) {
+	static ColorR buf[MAX_LEN];
+	for(int i=0; i<MAX_LEN - 1; ++i) {
+#define COMP(c) buf[i].c = color[i].c + (color[i+1].c - color[i].c) * x
+		COMP(r);
+		COMP(g);
+		COMP(b);
+#undef COMP
+	}
+	int len = MAX_LEN - 1;
+	while(len > 1) {
+#define COMP(c) buf[i].c = buf[i].c + (buf[i+1].c - buf[i].c) * x
+		for(int i=0; i<len-1; ++i) {
+			COMP(r);
+			COMP(g);
+			COMP(b);
+		}
+		len--;
+#undef COMP
+	}
+	ColorI res;
+	res.r = (int)buf[0].r;
+	res.g = (int)buf[0].g;
+	res.b = (int)buf[0].b;
+	return res;
+//	int r = ((int)buf[0].r) * 0xff * 0xff;
+//	int g = ((int)buf[0].g) * 0xff;
+//	int b = (int)buf[0].b;
+//	return r | g | b;
+}
+
+void null_clr() {
+	color_len = 0;
+}
+
+void add_clr(int r, int g, int b) {
+	color[color_len].r = (double)r;
+	color[color_len].g = (double)g;
+	color[color_len].b = (double)b;
+	color_len = (color_len + 1) % MAX_LEN;
+}
+
 void init_win(int w, int h, double x, double X, double y, double Y, int d) {
 	dx = X - x;
 	min_x = x;
@@ -35,10 +93,10 @@ void init_win(int w, int h, double x, double X, double y, double Y, int d) {
 	height = (double)h;
 }
 
-double calc_point(int px, int py) {
+ColorI point_color(int px, int py) {
 	double x = (double)px / width;
 	double y = (double)py / height;
 	x = min_x + x * dx;
 	y = min_y + y * dy;
-	return julia(x,y) / depth;
+	return get_color(julia(x,y) / depth);
 }
--- mandelbort.py
+++ mandelbort.py
@@ -5,25 +5,32 @@ import sys
 
 # use np.longdouble for crd
 
-black = (0,0,0)
-white = (255,255,255)
+colors = [
+            (0,0,0),
+            (0,0,255),
+            (255,255,0),
+            (255,255,255)
+        ]
 
 depth = 60
-
-def color_num(x):
-    return (int(x * 255), int(x * 255), int(x * 255))#int((1 - x) * 255))
 
 def draw_c(width,height,x,X,y,Y):
     img = Image.new('RGB', (width, height), 'white')
     pic = img.load()
     ffi = cffi.FFI()
+    ffi.cdef('struct Color{int r; int g; int b;};')
     ffi.cdef('void init_win(int w, int h, double x, double X, double y, double Y, int d);')
-    ffi.cdef('double calc_point(int px, int py);')
+    ffi.cdef('void null_clr();')
+    ffi.cdef('void add_clr(int r, int g, int b);')
+    ffi.cdef('struct Color point_color(int px, int py);')
     lib = ffi.dlopen('./libcalc.so')
+    for (r,g,b) in colors:
+        lib.add_clr(r,g,b)
     lib.init_win(width,height,x,X,y,Y,depth)
     for x in range(width):
         for y in range(height):
-            pic[x,y] = color_num(lib.calc_point(x,y))
+            clr = lib.point_color(x,y)
+            pic[x,y] = (clr.r, clr.g, clr.b)
     img.save('pic.png')
 
-draw_c(1024,768,-2,2,-2,2)
+draw_c(500,500,-2,2,-2,2)
